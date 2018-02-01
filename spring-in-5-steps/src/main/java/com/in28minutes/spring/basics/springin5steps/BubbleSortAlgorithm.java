76eb0f027c122b67980a1344eb82fd4a0e989d27
mirko <mirko.djurdjevic@ymail.com>
2018-02-01 11:53:26 +0100

    Add a second component

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // used to tell Spring to go with this component first
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
     @Override
