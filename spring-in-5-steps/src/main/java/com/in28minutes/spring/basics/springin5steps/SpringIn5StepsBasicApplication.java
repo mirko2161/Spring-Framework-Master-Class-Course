af53879fde09caf69255336774ad734884a37e05
mirko <mirko.djurdjevic@ymail.com>
2018-02-02 13:34:39 +0100

    Convert Basic App to use Core Spring only

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,17 +1,21 @@
 package com.in28minutes.spring.basics.springin5steps;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
 
-@SpringBootApplication // Automatically scans the package and it's subpackages
+@Configuration
+@ComponentScan
 public class SpringIn5StepsBasicApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                SpringIn5StepsBasicApplication.class);
+
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
         BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
 
