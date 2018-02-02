8d2db15be3a07e0d2d356a45b778e4ee09e4a2d1
mirko <mirko.djurdjevic@ymail.com>
2018-02-02 13:53:57 +0100

    Fix logger dependency and close app context

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,6 +1,5 @@
 package com.in28minutes.spring.basics.springin5steps;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +12,19 @@ public class SpringIn5StepsBasicApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-                SpringIn5StepsBasicApplication.class);
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                SpringIn5StepsBasicApplication.class)) {
 
-        BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-        BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+            BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+            BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
 
-        System.out.println(binarySearch); // by default same objects
-        System.out.println(binarySearch2);
+            System.out.println(binarySearch); // by default same objects
+            System.out.println(binarySearch2);
 
-        int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-        System.out.println(result);
+            int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+            System.out.println(result);
+
+        }
 
     }
 
