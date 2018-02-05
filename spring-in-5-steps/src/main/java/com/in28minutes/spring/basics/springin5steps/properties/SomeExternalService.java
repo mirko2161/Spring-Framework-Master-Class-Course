69dbe7f0e678222827a3ec9553cfee9d28ef1fd6
mirko <mirko.djurdjevic@ymail.com>
2018-02-05 13:28:47 +0100

    Add extra configuration via properties

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/properties/SomeExternalService.java
@@ -0,0 +1,16 @@
+package com.in28minutes.spring.basics.springin5steps.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SomeExternalService {
+
+    @Value("${external.service.url}")
+    private String url; // we what this to be different in diff environments
+
+    public String returnServiceURL() {
+        return url;
+    }
+
+}
