1961f7bdc23f5538d6214b034b1ee6eaa996b79e
mirko <mirko.djurdjevic@ymail.com>
2018-02-07 15:10:18 +0100

    Add a View Resolver

--- test-web-application/src/main/java/com/in28minutes/springmvc/LoginController.java
+++ test-web-application/src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -2,15 +2,13 @@ package com.in28minutes.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = "/login")
-    @ResponseBody
     public String sayHello() {
-        return "Hello World";
+        return "login";
     }
 
 }
